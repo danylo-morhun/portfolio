839bc5f634edbfac43c694aa8d39f8b9cb1115a2
Danylo Morhun <danila.morgun@gmail.com>
2025-03-16 00:23:03 +0100

    feat: make header a component

---
+++ src/app/home/components/header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { RiUserSmileLine, RiWindowLine, RiMoonClearLine } from "react-icons/ri";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex items-center justify-between rounded-2xl bg-(--cards) w-full h-16 px-6 shadow-sm">
+      <div className="flex items-center gap-2">
+        <div className="rounded-full bg-(--primary) p-2 font-bold w-9 h-9 flex items-center justify-center">
+          DM
+        </div>
+        <RiUserSmileLine size={36} color="(--foreground)" />
+        <RiWindowLine size={36} color="(--foreground)" />
+      </div>
+      <div className="flex items-center gap-2">
+        <RiMoonClearLine size={36} color="(--foreground)" />
+        <button className="bg-(--primary) hover:bg-(--primary-50) text-(--foreground) font-bold py-2 px-4 rounded-xl">
+          Hire me!
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { RiUserSmileLine, RiWindowLine, RiMoonClearLine } from "react-icons/ri";
+import Header from "./components/header";
 
 const Home: React.FC = () => {
   return (
     <div className="bg-(--background) p-2 w-140 m-auto">
-      <header className="flex items-center justify-between rounded-2xl bg-(--cards) w-full h-16 px-6 shadow-sm">
-        <div className="flex items-center gap-2">
-          <div className="rounded-full bg-(--primary) p-2 font-bold w-9 h-9 flex items-center justify-center">
-            DM
-          </div>
-          <RiUserSmileLine size={36} color="(--foreground)" />
-          <RiWindowLine size={36} color="(--foreground)" />
-        </div>
-        <div className="flex items-center gap-2">
-          <RiMoonClearLine size={36} color="(--foreground)" />
-          <button className="bg-(--primary) hover:bg-(--primary-50) text-(--foreground) font-bold py-2 px-4 rounded-xl">
-            Hire me!
-          </button>
-        </div>
-      </header>
-
+      <Header />
       <section className="rounded-2xl bg-(--cards) w-full shadow-sm mt-2 pb-2">
         <div className="flex items-center justify-between p-6 w-full">
           <div className="flex items-center">
