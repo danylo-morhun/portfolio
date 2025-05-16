f6968c9c26085564cd60bcf71bb08c5b171d1689
Danylo Morhun <danylo-morhun@ezcloudllc.com>
2025-05-16 12:39:49 +0200

    fix: dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${spaceGrotesk.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
