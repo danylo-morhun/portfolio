d8ec3f6923b16a8e52d4919ed8c4421a05120093
Danylo Morhun <danymorhun@gmail.com>
2025-09-25 16:50:34 +0200

    ref: update seo and cv

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.danylomorhun.com/'
+  const baseUrl = 'https://www.danylomorhun.com'
   
   return [
     {
